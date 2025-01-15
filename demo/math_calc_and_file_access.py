d7673b04296357b453a0bc9a6955cfc04bae0963
吴子豪 <vortezwohl@proton.me>
2025-01-16 01:25:40 +0800

    update demo

--- demo/math_calc_and_file_access.py
+++ demo/math_calc_and_file_access.py
@@ -1,16 +1,15 @@
-import logging
-
 from ceo import (
     Agent,
     Personality,
     get_openai_model,
     ability
 )
+from ceo.brain.hook import BeforeActionTaken, AfterActionTaken
+from ceo.message import BeforeActionTakenMessage, AfterActionTakenMessage
 from sympy import simplify
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.getLogger('ceo').setLevel(logging.DEBUG)
 
 
 @ability
@@ -26,11 +25,21 @@ def write_file(filename: str, content: str) -> str:
     return f'{content} written to {filename}.'
 
 
+def before_action_taken(agent: Agent, message: BeforeActionTakenMessage):
+    print(f'Agent: {agent.name}, Next move: {message}')
+    return message
+
+
+def after_action_taken(agent: Agent, message: AfterActionTakenMessage):
+    print(f'Agent: {agent.name}, Action taken: {message}')
+    return message
+
+
 if __name__ == '__main__':
     ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='CEO', personality=Personality.INQUISITIVE)
     radius = '(10.01 * 10.36 * 3.33 / 2 * 16)'  # 2762.663904
     pi = 3.14159
     output_file = 'result.txt'
     request = f"Here is a sphere with radius of {radius} cm and pi here is {pi}, find the area and volume respectively then write the results into a file called '{output_file}'."
-    result = ceo.assign(request).just_do_it()  # area = 95910378.2949379, volume = 88322713378.13666
-    print(result)
+    result = ceo.assign(request).just_do_it(BeforeActionTaken(before_action_taken), AfterActionTaken(after_action_taken))  # area = 95910378.2949379, volume = 88322713378.13666
+    print(f'Result: {result}')
--- demo/multi_agent.py
+++ demo/multi_agent.py
@@ -1,5 +1,3 @@
-import logging
-
 from sympy import simplify
 from dotenv import load_dotenv
 from ceo import (
@@ -9,19 +7,20 @@ from ceo import (
     agentic,
     ability
 )
+from ceo.brain.hook import BeforeActionTaken, AfterActionTaken
+from ceo.message import BeforeActionTakenMessage, AfterActionTakenMessage
 
 load_dotenv()
-logging.getLogger('ceo').setLevel(logging.DEBUG)
 model = get_openai_model()
 
 
-@ability
+@ability(model)
 def calculator(expr: str) -> float:
     # this function only accepts a single math expression
     return simplify(expr)
 
 
-@ability
+@ability(model)
 def write_file(filename: str, content: str) -> str:
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
@@ -42,11 +41,21 @@ def agent2():
     return
 
 
+def before_action_taken(agent: Agent, message: BeforeActionTakenMessage):
+    print(f'Agent: {agent.name}, Next move: {message}')
+    return message
+
+
+def after_action_taken(agent: Agent, message: AfterActionTakenMessage):
+    print(f'Agent: {agent.name}, Action taken: {message}')
+    return message
+
+
 if __name__ == '__main__':
     ceo = Agent(abilities=[agent1, agent2], brain=model, name='CEO', personality=Personality.INQUISITIVE)
     radius = '(10.01 * 10.36 * 3.33 / 2 * 16)'  # 2762.663904
     pi = 3.14159
     output_file = 'result.txt'
     request = f"Here is a sphere with radius of {radius} cm and pi here is {pi}, find the area and volume respectively then write the results into a file called '{output_file}'."
-    result = ceo.assign(request).just_do_it()  # area = 95910378.2949379, volume = 88322713378.13666
-    print(result)
+    result = ceo.assign(request).just_do_it(BeforeActionTaken(before_action_taken), AfterActionTaken(after_action_taken))  # area = 95910378.2949379, volume = 88322713378.13666
+    print(f'Result: {result}')
