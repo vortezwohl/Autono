7e1bda9ea95f9534407c9004cb771c9be0606a86
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-15 16:26:14 +0800

    feat: 新增 NextMoveMessage

---
+++ ceo/message/next_move_message.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from typing_extensions import override
+
+from ceo.ability.ability import Ability
+from ceo.message.base_massage import BaseMessage
+
+
+@dataclass
+class NextMoveMessage(BaseMessage):
+    ability: Ability
+    arguments: dict
+
+    @override
+    def to_dict(self):
+        return {
+            'ability': self.ability.to_dict(),
+            'args': self.arguments
+        }
