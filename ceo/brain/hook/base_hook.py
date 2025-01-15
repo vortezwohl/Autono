a4688fae84dbb2ddf5829c691d01fcc846724a85
吴子豪 <vortezwohl@proton.me>
2025-01-15 22:44:46 +0800

    feat: hook 支持协程函数

--- ceo/brain/hook/base_hook.py
+++ ceo/brain/hook/base_hook.py
@@ -1,3 +1,6 @@
+import asyncio
+import inspect
+import threading
 from typing import Callable
 
 from ceo.message.base_massage import BaseMessage
@@ -10,6 +13,23 @@ class BaseHook(Callable[[BaseAgent, BaseMessage], BaseMessage]):
         self._function = function
 
     def __call__(self, agent: BaseAgent, message: BaseMessage):
+        if inspect.iscoroutinefunction(self._function):
+            __res = None
+
+            def __func(loop: asyncio.AbstractEventLoop):
+                nonlocal __res, agent, message
+                try:
+                    __res = loop.run_until_complete(self._function(agent, message))
+                finally:
+                    loop.close()
+
+            __thread = threading.Thread(
+                target=__func,
+                args=(asyncio.new_event_loop(),)
+            )
+            __thread.start()
+            __thread.join(timeout=None)
+            return __res
         return self._function(agent, message)
 
     @staticmethod
