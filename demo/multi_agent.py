96efc315947859925733924b13dd90a993d70e43
吴子豪 <vortezwohl@proton.me>
2025-03-03 09:17:24 +0800

    dataset: 创建并保存next_move_dataset

--- demo/multi_agent.py
+++ demo/multi_agent.py
@@ -10,7 +10,10 @@ from ceo import (
 from ceo.brain.hook import BeforeActionTaken, AfterActionTaken
 from ceo.message import BeforeActionTakenMessage, AfterActionTakenMessage
 
+from duckduckgo_search import DDGS
+
 load_dotenv()
+ddgs = DDGS()
 model = get_openai_model()
 
 
@@ -25,6 +28,33 @@ def write_file(filename: str, content: str) -> str:
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
     return f'{content} written to {filename}.'
+
+
+@ability
+def search_on_duckduckgo(keywords: list[str], max_results_for_each_keywords: int = 3) -> dict:
+    # search on duckduckgo if you don't know clearly about something
+    # you can search several keywords at the same time with `search_on_duckduckgo`
+    results = dict()
+    for kw in keywords:
+        search_results = ddgs.text(
+            keywords=kw,
+            region='wt-wt',
+            safesearch='off',
+            timelimit=None,
+            backend='auto',
+            max_results=max_results_for_each_keywords
+        )
+        for _res in search_results:
+            _title = _res.get('title', 'unknown')
+            _res.pop('title')
+            _link = _res.get('href', 'unknown')
+            _res.pop('href')
+            _res['source'] = f'({_title})[{_link}]'
+            _content = _res.get('body')
+            _res.pop('body')
+            _res['content'] = _content
+        results[f'search results for "{kw}"'] = search_results
+    return results
 
 
 jack = Agent(abilities=[calculator], brain=model, name='Jack', personality=Personality.INQUISITIVE)
