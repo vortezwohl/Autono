afd1e3600931f22fdbb91b5d85b02f34bb92f2aa
吴子豪 <vortezwohl@proton.me>
2025-03-26 23:28:55 +0800

    update demos 新增TTS试验性增强

---
+++ demo/math_calc_and_file_access_tts_enhanced.py
@@ -0,0 +1,55 @@
+from ceo import (
+    Agent,
+    Personality,
+    get_openai_model,
+    ability
+)
+from ceo.brain.hook import BeforeActionTaken, AfterActionTaken
+from ceo.message import BeforeActionTakenMessage, AfterActionTakenMessage
+from sympy import simplify
+from dotenv import load_dotenv
+import pyttsx3
+
+tts_engine = pyttsx3.init()
+tts_engine.setProperty('rate', 150)
+tts_engine.setProperty('volume', 1.0)
+tts_engine.setProperty('voice', tts_engine.getProperty('voices')[3].id)
+load_dotenv()
+
+
+@ability
+def calculator(expr: str) -> float:
+    # this function only accepts a single math expression
+    return simplify(expr)
+
+
+@ability
+def write_file(filename: str, content: str) -> str:
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+    return f'{content} written to {filename}.'
+
+
+def before_action_taken(agent: Agent, message: BeforeActionTakenMessage):
+    print(f'Agent: {agent.name}, Next move: {message}')
+    return message
+
+
+def after_action_taken(agent: Agent, message: AfterActionTakenMessage):
+    print(f'Agent: {agent.name}, Action taken: {message}')
+    tts_engine.say(message.summarization)
+    tts_engine.runAndWait()
+    return message
+
+
+if __name__ == '__main__':
+    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='CEO', personality=Personality.INQUISITIVE)
+    radius = '(10.01 * 10.36 * 3.33 / 2 * 16)'  # 2762.663904
+    pi = 3.14159
+    output_file = 'result.txt'
+    request = f"Here is a sphere with radius of {radius} cm and pi here is {pi}, find the area and volume respectively then write the results into a file called '{output_file}'."
+    result = ceo.assign(request).just_do_it(BeforeActionTaken(before_action_taken), AfterActionTaken(after_action_taken))  # area = 95910378.2949379, volume = 88322713378.13666
+    print(f'Result: {result}')
+    tts_engine.say(result.conclusion)
+    tts_engine.runAndWait()
+    tts_engine.stop()
