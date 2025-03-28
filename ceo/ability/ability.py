22325ebbb61181eb60c4493439b61296a3fab809
吴子豪 <vortez.wohl@gmail.com>
2025-03-28 19:51:20 +0800

    feat: Ability 现在有一个父类, 父类是 BaseAbility

--- ceo/ability/ability.py
+++ ceo/ability/ability.py
@@ -3,10 +3,12 @@ import inspect
 import json
 import threading
 
-from typing_extensions import Callable
+from typing_extensions import Callable, override
+
+from ceo.ability.base_ability import BaseAbility
 
 
-class Ability:
+class Ability(BaseAbility):
     def __init__(self, function: Callable):
         signature = inspect.signature(function)
         doc_str = inspect.getdoc(function)
@@ -14,25 +16,24 @@ class Ability:
             doc_str = json.dumps({
                 'src': inspect.getsource(function)
             }, ensure_ascii=False)
-        self._name: str = function.__name__
-        self._description: str | dict = str()
+        _description: str | dict = str()
         self._function: Callable = function
-        self._parameters: dict = dict()
-        self._returns: any = signature.return_annotation
+        _parameters: dict = dict()
         for name, param in signature.parameters.items():
-            self._parameters[name] = str(param.annotation)
+            _parameters[name] = str(param.annotation)
         try:
-            self._description = json.loads(doc_str)
-            self._description = self._description.get('description', self._description)
+            _description = json.loads(doc_str)
+            _description = _description.get('description', _description)
         except json.decoder.JSONDecodeError:
-            self._description = doc_str
+            _description = doc_str
+        super().__init__(
+            name=function.__name__,
+            description=_description,
+            parameters=_parameters,
+            returns=signature.return_annotation
+        )
 
-    def __repr__(self):
-        return json.dumps(self.to_dict(), ensure_ascii=False)
-
-    def __str__(self):
-        return self.__repr__()
-
+    @override
     def __call__(self, *args, **kwargs):
         if inspect.iscoroutinefunction(self._function):
             __res = None
@@ -52,35 +53,6 @@ class Ability:
             __thread.join(timeout=None)
             return __res
         return self._function(*args, **kwargs)
-
-    def to_dict(self) -> dict:
-        param_list: list = list()
-        unnecessary_params: tuple = ('args', 'kwargs')
-        for name, _ in self._parameters.items():
-            if name not in unnecessary_params:
-                param_list.append(name)
-        return {
-            'ability_name': self._name,
-            'description': self._description,
-            'parameters_required': param_list,
-            'returns': str(self._returns)
-        }
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def description(self) -> str:
-        return self._description
-
-    @property
-    def parameters(self) -> dict:
-        return self._parameters
-
-    @property
-    def returns(self) -> any:
-        return self._returns
 
     @property
     def function(self) -> Callable:
