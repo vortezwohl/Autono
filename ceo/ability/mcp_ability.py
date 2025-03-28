adb8ce2bb64151cdc754695968477906a5d35fea
吴子豪 <vortez.wohl@gmail.com>
2025-03-28 17:54:38 +0800

    mcp tool 格式转换 生成 docstring

--- ceo/ability/mcp_ability.py
+++ ceo/ability/mcp_ability.py
@@ -1,3 +1,5 @@
+import json
+
 from mcp.types import Tool
 from typing_extensions import override
 
@@ -5,7 +7,7 @@ from ceo.ability.base_ability import BaseAbility
 from ceo.exception.invalid_mcp_tool_exception import InvalidMcpToolException
 
 UNKNOWN = 'Unknown'
-NONE = 'None'
+NONE = '/'
 
 
 # todo 封装 Mcp tool
@@ -15,18 +17,27 @@ class McpAbility(BaseAbility):
         _input_schema_dict = mcp_tool.inputSchema
         if 'properties' not in _input_schema_dict.keys() or not isinstance(_input_schema_dict.get('properties'), dict):
             raise InvalidMcpToolException(mcp_tool=mcp_tool)
-        _params = [
-            {
-                'name': k,
-                'type': 'str',
-                'description': v.get('description', NONE)
-            } for k, v in _input_schema_dict['properties'].items()
-        ]
-        _tmp_doc = {
-            'brief_description': mcp_tool.description,
-            'parameters': mcp_tool.inputSchema
-        }
+        self.__doc__ = json.dumps({'brief_description': mcp_tool.description, 'parameters': [
+            {'name': k, 'type': 'str', 'description': v.get('description', NONE)}
+            for k, v in _input_schema_dict['properties'].items()
+        ]}, ensure_ascii=False)
 
     @override
     def __call__(self, *args, **kwargs):
         ...
+
+
+if __name__ == '__main__':
+    tool = Tool(
+        name="playwright_screenshot",
+        description="Take a screenshot of the current page or a specific element",
+        inputSchema={
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "selector": {"type": "string", "description": "CSS selector for element to screenshot,null is full page"},
+            },
+            "required": ["name"]
+        }
+    )
+    McpAbility(tool)
