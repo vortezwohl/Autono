f033402a9a9e17167031020d41a6a9fa8a0a127e
吴子豪 <vortez.wohl@gmail.com>
2025-03-31 15:54:43 +0800

    update: 优化 sdk 导出

--- ceo/brain/lm/__init__.py
+++ ceo/brain/lm/__init__.py
@@ -1,3 +1,15 @@
-from .openai import get_lm as get_openai_model
-from .dashscope import get_lm as get_dashscope_model
-from .deepseek import get_lm as get_deepseek_model
+from .openai import get_lm as __get_openai_model, BaseChatModel
+from .dashscope import get_lm as __get_dashscope_model
+from .deepseek import get_lm as __get_deepseek_model
+
+
+def get_openai_model(*args, **kwargs) -> BaseChatModel:
+    return __get_openai_model(*args, **kwargs)
+
+
+def get_dashscope_model(*args, **kwargs) -> BaseChatModel:
+    return __get_dashscope_model(*args, **kwargs)
+
+
+def get_deepseek_model(*args, **kwargs) -> BaseChatModel:
+    return __get_deepseek_model(*args, **kwargs)
